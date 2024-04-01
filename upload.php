087b15cc3f522b8312af5fdea7034fff127a5265
Martín Silveira de Almeida <116463214+Martin-misogino@users.noreply.github.com>
2024-04-01 15:52:45 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -3,5 +3,8 @@
 $pastaDestino = "/uploads/";
 $nomeArquivo = $_FILES['arquivo']['name'];
 var_dump($_FILES['arquivo']['name']);
-file_exists(__DIR__. $pastaDestino. $nomeArquivo);
+if (file_exists(__DIR__. $pastaDestino. $nomeArquivo)) {
+    echo "Arquivo já existe";
+    exit;
+}
 var_dump(__DIR__. $pastaDestino. $nomeArquivo); 
