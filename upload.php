8cbb52cdcfae0b7d35c7100b596840d8f79be32b
Martín Silveira de Almeida <116463214+Martin-misogino@users.noreply.github.com>
2024-04-01 15:48:13 -0300

    updateado

--- upload.php
+++ upload.php
@@ -3,3 +3,5 @@
 $pastaDestino = "uploads/";
 $nomeArquivo = $_FILES['arquivo']['name'];
 var_dump($_FILES['arquivo']['name']);
+file_exists(__DIR__. $pastaDestino. $nomeArquivo);
+var_dump(__DIR__. $pastaDestino. $nomeArquivo);
