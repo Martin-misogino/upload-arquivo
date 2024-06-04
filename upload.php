803890a71c7244da2c191c2f1503bc29f9b39a09
Martín Silveira de Almeida <martin.2022311000@aluno.iffar.edu.br>
2024-06-04 17:08:01 -0300

    verlei

--- upload.php
+++ upload.php
@@ -22,9 +22,23 @@ exit;
 
 //var_dump (basename($_FILES["arquivo"]["name"]));
 $extensao = strtolower(pathinfo($nomeArquivo,PATHINFO_EXTENSION));
-var_dump (strtolower(pathinfo($target_file,PATHINFO_EXTENSION)));
+//var_dump (strtolower(pathinfo($target_file,PATHINFO_EXTENSION)));
 
 if($extensao != "jpg" and $extensao != "jpeg" and $extensao != "png" and $extensao != "gif"){
  echo "formato do arquivo invalido";
  exit;
+}
+if(getimagesize($_FILES['arquivo']['tmp_name']) === false){
+    echo"problemas ao enviar a imagem tente novamente";
+}
+//se deu tudo certo até aqui faz o upload
+var_dump($pastaDestino. $_FILES['arquivo']['name']);
+var_dump(__DIR__.$pastaDestino. $_FILES['arquivo']['name']);
+$fezUpload= move_uploaded_file($_FILES['arquivo']['tmp_name'],__DIR__$pastaDestino. $_FILES['arquivo']['name']);
+
+if($fezUpload == true){
+    header("location:index.php");
+}
+else{
+    echo "Erro ao mover o arquivo.";
 }
